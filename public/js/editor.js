671fe02633400beebeed9577edb1cef7b29e7673
Timo Saarinen <tsaari42@gmail.com>
2024-04-17 20:51:41 +0300

    Editor tweaks

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,61 +1,16 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   const editor = CodeMirror.fromTextArea(document.getElementById('code'), {
-//     mode: 'your-language-mode', // define this if custom
-//     lineNumbers: true
-//   });
-//
-//   editor.on('change', function() {
-//     // Fetch and display compilation output on each change
-//     fetch('/compile', {
-//       method: 'POST',
-//       headers: { 'Content-Type': 'application/json' },
-//       body: JSON.stringify({ code: editor.getValue() })
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//       document.getElementById('output').textContent = data.output;
-//     });
-//   });
-// });
-
-// --------------------
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   const editor = CodeMirror(document.getElementById('editor'), {
-//       mode: 'javascript',  // Change this to your custom mode if you have one
-//       theme: 'dracula',
-//       lineNumbers: true
-//   });
-
-//   editor.setSize("100%", "100%");
-
-//   editor.on('change', function() {
-//       fetch('/compile', {
-//           method: 'POST',
-//           headers: { 'Content-Type': 'application/json' },
-//           body: JSON.stringify({ code: editor.getValue() })
-//       })
-//       .then(response => response.json())
-//       .then(data => {
-//           document.getElementById('output').textContent = data.output;
-//       })
-//       .catch(error => {
-//           document.getElementById('output').textContent = 'Error: ' + error;
-//       });
-//   });
-// });
-
-document.addEventListener('DOMContentLoaded', function() {
-  const initialCode = `// Start typing your code here
+const initialCode = `
+// Start typing your code here
 print 'Hello, world!'`;
 
+//------------------------------------------------------------------------
+document.addEventListener('DOMContentLoaded', function() {
+
   const editor = CodeMirror(document.getElementById('editor'), {
-      mode: 'javascript',  // Change this to your custom mode if you have one
+      mode: 'javascript',
       theme: 'dracula',
       lineNumbers: true,
-      value: initialCode // Set the initial content of the editor
+      value: initialCode
   });
-
   editor.setSize("100%", "100%");
 
   editor.on('change', function() {
