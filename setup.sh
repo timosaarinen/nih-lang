20dbc3695b853ff043477f6b726effdd7d24d8b9
Timo Saarinen <tsaari42@gmail.com>
2024-04-29 01:06:34 +0300

    Fixed typo

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ mkdir ~/nih-1.0.0
 cp -R ./dist/* ~/nih-1.0.0
 
 # Add 'nih' to PATH
-echo "Adding 'nih' to PATH.."
-export PATH="$PATH:~/nih"
+echo "Adding ~/nih-1.0.0 to PATH.."
+export PATH="$PATH:~/nih-1.0.0"
 echo "Your PATH is now: " $PATH
 echo "If you ran this with 'source setup.sh' you should now be able to write and run 'nih'!"
